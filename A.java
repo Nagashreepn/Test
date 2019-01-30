e5a427fb0fe32fa3d673576870f29739e170cee7
nagashree <nagashree1606@gmail.com>
2019-01-30 11:54:31 +0530

    This is a modified file in Test stage

--- A.java
+++ A.java
@@ -2,6 +2,7 @@ class A {
 
 public static void main(String[] args){
 
+System.out.println("This is a A class");
 System.out.println("This is a demo class");
 System.out.println("This is a demo1 class");
 }
